780a85609cf239eccbb7c3022839d872eda85ab1
Piotr Maślankowski <pmaslankowski@gmail.com>
2017-02-06 00:57:27 +0100

    Sprite bugs fixed.

--- TanX/Sprite.cpp
+++ TanX/Sprite.cpp
@@ -2,25 +2,24 @@
 
 
 void Sprite::setOrientation(const Vector& v) {
-	float alpha = getRotation();
-
 	// scalar to transform from radians to degrees
 	const float transformScalar = 180.0f / 3.14159265358979f;
 
 	// cos angle between two vector is equal to their dot product divided by their magnitudes
 	// we're performing dot product on reflected vector, because screen coordinate system is left-handed
-	// this is why there is minus sign in formula below
-	float cosDeltaAlpha = (v.x * sin(alpha) - v.y * cos(alpha)) / sqrt(v.x * v.x + v.y * v.y);
-	float deltaAlpha = transformScalar * acos(cosDeltaAlpha);
+	// we want to know angle from vertical line, so we're computing dot product of v and [0, -1]
+	float cosAlpha = (0.0f * v.x - 1.0f * v.y) / sqrt(v.x * v.x + v.y * v.y);
+	float alpha = transformScalar * acos(cosAlpha);
 
 	// we have to check what quater of coordinates system is given vector in
 	if (v.x > 0)
-		setRotation(alpha + deltaAlpha);
+		setRotation(alpha);
 	else
-		setRotation(alpha + 360.0f - deltaAlpha);
+		setRotation(360.f - alpha);
 }
+
 
 Vector Sprite::getOrientation() const {
 	float alpha = getRotation();
-	return Vector(cos(alpha), sin(alpha));
+	return Vector(sin(alpha), -cos(alpha));
 }
